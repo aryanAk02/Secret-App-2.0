34ad0bde7e9f7d2cc6d01aa745aaf42956a9dd22
aryanAk02 <aryanakash008@gmail.com>
2023-09-06 18:06:13 +0530

    level 4 authentication and security

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser=require("body-parser");
 const ejs = require("ejs");
 const mongoose=require("mongoose");
 const md5=require("md5");
+const bcrypt=require("bcrypt");
+const saltRounds=10;
 
 
 const app=express();
@@ -40,26 +42,30 @@ app.get("/register",function(req,res){
 })
 
 app.post("/register",function(req,res){
-    const newUser=new User({
-        email : req.body.username,
-        password:md5(req.body.password)
+    bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+        const newUser=new User({
+            email : req.body.username,
+            password:hash
+        });
+        console.log(newUser);
+        
+        newUser.save();
+        res.redirect("/");
     });
-    console.log(newUser);
-    
-    newUser.save();
-    res.redirect("/");
-    
 })
 
 app.post("/login",function(req,res){
     const username=req.body.username;
-    const password=md5(req.body.password);
+    const password=req.body.password;
+    
     User.findOne({email:username}).then(foundUser=>{
-        if(!foundUser) {
-            console.log("user not found");
+        if(foundUser) {
+            bcrypt.compare(password,foundUser.password,function(err,result){
+                if(result===true)
+                    res.render("secrets");
+            });
         }else{
-            if(foundUser.password===password)
-                res.render("secrets");
+            console.log("user not found");
         }
     })
 })
